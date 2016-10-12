8c1b11ffcae68917d7db9b236ba0086e3c48d335
songchenyu <2359480792@qq.com>
2016-10-12 15:16:03 +0800

    update string

--- src/main/java/com/wodm/android/Constants.java
+++ src/main/java/com/wodm/android/Constants.java
@@ -22,10 +22,10 @@ public class Constants {
 
     public static UserInfoBean CURRENT_USER/* = new UserBean()*/;
 
-      // public final static String HOST = "http://wodm.9mobi.cn/api/v1/";// 正式
-   // public final static String HOST = "http://172.16.2.125:8899/api/v1/";// 公司测试服务器
+    // public final static String HOST = "http://wodm.9mobi.cn/api/v1/";// 正式
+    public final static String HOST = "http://172.16.2.125:8899/api/v1/";// 公司测试服务器
 //  public final static String HOST = "http://59.108.94.55:8899/api/v1/";// 公司外网服务器
-   public final static String HOST = "http://172.16.3.49:8080/api/v1/";//  小康子
+//    public final static String HOST = "http://172.16.3.49:8080/api/v1/";//  小康子
 
     //分享详情页
     public final static String SHARE_URL = "http://wodm.9mobi.cn/html/donghuaxiangqingye.html?id=";
