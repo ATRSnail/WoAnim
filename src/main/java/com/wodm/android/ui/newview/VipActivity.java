40ce56eda0ddc67a0d354e32c62c44e1c5a4a259
songchenyu <2359480792@qq.com>
2016-10-14 09:45:12 +0800

    update vip

--- src/main/java/com/wodm/android/ui/newview/VipActivity.java
+++ src/main/java/com/wodm/android/ui/newview/VipActivity.java
@@ -1,51 +1,48 @@
 package com.wodm.android.ui.newview;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.wodm.R;
-import com.wodm.android.bean.UserInfoBean;
 import com.wodm.android.ui.AppActivity;
 import com.wodm.android.view.newview.AtyTopLayout;
 
 import org.eteclab.base.annotation.Layout;
 import org.eteclab.base.annotation.ViewIn;
-import org.eteclab.base.utils.AsyncImageLoader;
-import org.eteclab.ui.widget.CircularImage;
-
-import static com.wodm.android.Constants.CURRENT_USER;
 
 /**
  * Created by songchenyu on 16/10/8.
  */
-@Layout(R.layout.aty_vip)
+@Layout(R.layout.aty_new_vip)
 public class VipActivity extends AppActivity implements AtyTopLayout.myTopbarClicklistenter {
-    @ViewIn(R.id.user_head_imgs)
-    private CircularImage user_head_imgs;
-    @ViewIn(R.id.vip_user_head_imgs)
-    private CircularImage vip_user_head_imgs;
-    @ViewIn(R.id.tv_vip_name)
-    private TextView tv_vip_name;
+//    @ViewIn(R.id.user_head_imgs)
+//    private CircularImage user_head_imgs;
+//    @ViewIn(R.id.vip_user_head_imgs)
+//    private CircularImage vip_user_head_imgs;
+//    @ViewIn(R.id.tv_vip_name)
+//    private TextView tv_vip_name;
     @ViewIn(R.id.set_topbar)
     private AtyTopLayout set_topbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (CURRENT_USER == null) {
-            finish();
-            return;
-        }
+//        if (CURRENT_USER == null) {
+//            finish();
+//            return;
+//        }
         set_topbar.setOnTopbarClickListenter(this);
-        UserInfoBean.DataBean.AccountBean accountBean=CURRENT_USER.getData().getAccount();
-        if (accountBean.getVip()==0){
-            vip_user_head_imgs.setBackgroundResource(R.mipmap.circle_novip);
-        }else{
-            vip_user_head_imgs.setBackgroundResource(R.mipmap.circlr_vip);
-        }
-        tv_vip_name.setText(accountBean.getNickName());
-        new AsyncImageLoader(this, R.mipmap.default_header, R.mipmap.default_header).display(user_head_imgs, CURRENT_USER.getData().getAccount().getPortrait());
+
     }
+//   private void setUserInfo(){
+//       UserInfoBean.DataBean.AccountBean accountBean=CURRENT_USER.getData().getAccount();
+//       if (accountBean.getVip()==0){
+//           vip_user_head_imgs.setBackgroundResource(R.mipmap.circle_novip);
+//       }else{
+//           vip_user_head_imgs.setBackgroundResource(R.mipmap.circlr_vip);
+//       }
+//       tv_vip_name.setText(accountBean.getNickName());
+//       new AsyncImageLoader(this, R.mipmap.default_header, R.mipmap.default_header).display(user_head_imgs, CURRENT_USER.getData().getAccount().getPortrait());
+//    }
 
     @Override
     public void leftClick() {
