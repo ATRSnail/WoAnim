f4efc35cb9775cfe5d8462854529fb4386a7455d
xuchunhui <252243862@qq.com>
2016-12-05 11:08:50 +0800

    发送评论页面

--- src/main/java/com/wodm/android/ui/newview/SendMsgActivity.java
+++ src/main/java/com/wodm/android/ui/newview/SendMsgActivity.java
@@ -1,17 +1,27 @@
 package com.wodm.android.ui.newview;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.wodm.R;
 import com.wodm.android.ui.AppActivity;
 import com.wodm.android.view.newview.AtyTopLayout;
 
 import org.eteclab.base.annotation.Layout;
+import org.eteclab.base.annotation.ViewIn;
 
 @Layout(R.layout.activity_send_msg)
 public class SendMsgActivity extends AppActivity implements AtyTopLayout.myTopbarClicklistenter,
         View.OnClickListener{
+
+    @ViewIn(R.id.ed_comment)
+    EditText commentEd;
+    @ViewIn(R.id.tv_num)
+    TextView numTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,23 @@ public class SendMsgActivity extends AppActivity implements AtyTopLayout.myTopba
     }
 
     private void initViews() {
+
+        commentEd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                numTv.setText("剩余"+(150-s.length())+"字");
+            }
+        });
     }
 
     @Override
